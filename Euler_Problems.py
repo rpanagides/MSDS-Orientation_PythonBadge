4d9a35303983b0db0360319ebe3cadb88d70fca1
Reanna Panagides <reannapanagides@gmail.com>
2023-06-18 13:08:53 -0400

    deleting imports from top

--- Euler_Problems.py
+++ Euler_Problems.py
@@ -1,7 +1,3 @@
-import matplotlib
-import numpy as np
-import pandas as pd
-
 #Problem 1:
 
 multiple_sum = 0
@@ -40,7 +36,6 @@ n = 3
 while n < x/2:
     if x % n == 0:
         x = x / n
-        print(x)
     n += 1
 
-print(x) 
+#print(x) 
